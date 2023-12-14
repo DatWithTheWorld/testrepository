0203c451071a8f8ce661b937d67703d794555931
YUPP4 <nguyenducdat592004@gmail.com>
2023-12-14 07:59:59 +0700

    Up code

---
+++ src/FirstExercise/Hello.java
@@ -0,0 +1,14 @@
+package FirstExercise;
+
+import java.util.Scanner;
+
+public class Hello{
+
+	public static void main(String[] args) {
+	String ch;
+	Scanner sc = new Scanner(System.in);
+	ch =sc.nextLine();
+	System.out.println("Length: "+ch.length());
+	}
+
+}
